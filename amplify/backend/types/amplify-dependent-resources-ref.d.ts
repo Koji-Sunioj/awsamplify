a94f6652dd92846cb05aa8f2dc9cc4c96b6b4e21
Koji-Sunioj <koji.gabriel218@gmail.com>
2022-07-27 17:32:16 +0300

    first

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "amplifydemocb69680a": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "apia2d2c274": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
